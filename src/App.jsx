62d126c89c4a81f62aa5484b46f2d5a72d1d234c
Awolad Hossain <awoladh04@gmail.com>
2025-04-18 12:48:33 +0600

    updated state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./components/Form";
 import MemoryCard from "./components/MemoryCard";
 
 const App = () => {
   const [isGameOn, setIsGameOn] = useState(false);
   const [emojisData, setEmojisData] = useState([]);
-  console.log(emojisData);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  console.log(matchedCards);
+  useEffect(() => {
+    if (
+      selectedCards.length === 2 &&
+      selectedCards[0].name === selectedCards[1].name
+    ) {
+      setMatchedCards((prevMatchedCards) => [
+        ...prevMatchedCards,
+        ...selectedCards,
+      ]);
+    }
+  }, [selectedCards]);
+
   async function startGame(e) {
     e.preventDefault();
     try {
@@ -16,20 +30,63 @@ const App = () => {
         throw new Error("Failed to fetch the API");
       }
       const result = await response.json();
-      // console.log(result);
-      const dataSample = result.slice(0, 5);
-      // console.log(dataSample)
-      setEmojisData(dataSample);
-
+      const dataSlice = await getDataSlice(result);
+      const emojisArray = await getEmojisArray(dataSlice);
+      setEmojisData(emojisArray);
       setIsGameOn(true);
     } catch (error) {
       console.log("you got an error: ", error);
     }
   }
+  const getDataSlice = async (data) => {
+    const randomIndices = getRandomIndices(data);
+    const dataSlice = randomIndices.map((item) => data[item]);
+    //  const dataSlice = randomIndices.reduce((array, index) => {
+    //    array.push(data[index]);
+    //    return array;
+    //  }, []);
+    return dataSlice;
+  };
+  const getRandomIndices = (data) => {
+    let randomIndicesArray = [];
+    for (let i = 0; i < 5; i++) {
+      const randomNumber = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNumber)) {
+        randomIndicesArray.push(randomNumber);
+      } else {
+        i--;
+      }
+    }
+    return randomIndicesArray;
+  };
 
-  function turnCard() {
-    console.log("Memory card clicked");
+  const getEmojisArray = async (data) => {
+    const pairedEmojisArray = [...data, ...data];
+    // * Fisher-Yates algorithm
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = temp;
+    }
+    return pairedEmojisArray;
+  };
+
+  function turnCard(name, index) {
+    const selectedCardEntry = selectedCards.find(
+      (emoji) => emoji.index === index
+    );
+
+    if (!selectedCardEntry && selectedCards.length < 2) {
+      setSelectedCards((prevSelectedCards) => [
+        ...prevSelectedCards,
+        { name, index },
+      ]);
+    } else if (!selectedCardEntry && selectedCards.length === 2) {
+      setSelectedCards([{ name, index }]);
+    }
   }
+
   return (
     <main>
       <h1>Memory</h1>
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -13,10 +13,13 @@ const MemoryCard = ({ handleClick, data }) => {
   //   "🐛",
   //   "🐵",
   // ];
-  console.log(data);
+  // console.log(data);
   const emojiEl = data.map((emoji, index) => (
     <li key={index} className="card-item">
-      <button className="btn btn--emoji" onClick={handleClick}>
+      <button
+        className="btn btn--emoji"
+        onClick={() => handleClick(emoji.name, index)}
+      >
         {decodeEntity(emoji.htmlCode[0])}
       </button>
     </li>
